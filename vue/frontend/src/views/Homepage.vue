99314f5b387e62786190984343dd9ccc501833eb
Brian Berryman <bberryman614@gmail.com>
2020-04-11 07:56:40 -0400

    push

--- vue/frontend/src/views/Homepage.vue
+++ vue/frontend/src/views/Homepage.vue
@@ -1,12 +1,32 @@
 <template>
 <body>
+    <section class="headerMain">
+    </section>
+<section class="bodyMain">
+<carousel>
+  <slide>
+    Slide 1 Content
+  </slide>
+  <slide>
+    Slide 2 Content
+  </slide>
+</carousel>
+</section>
 
 
 </body>
 </template>
 
 <script>
-  
+  import { Carousel, Slide } from 'vue-carousel';
+export default {
+  ...
+  components: {
+    Carousel,
+    Slide
+  }
+  ...
+};
 export default {
     name: 'home',
     data() {
